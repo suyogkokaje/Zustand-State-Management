fe752d475966898e3fc4509505c2f1711f36b4cd
suyogkokaje <suyogkokaje6@gmail.com>
2023-02-04 12:49:38 +0530

    feat:added store

---
+++ src/app/store.js
@@ -0,0 +1,31 @@
+import {create} from 'zustand'
+import { devtools, persist } from 'zustand/middleware'
+
+const courseStore = (set) => ({
+    courses: [],//initial state
+
+
+    //crud actions
+
+    addCourse: (course) => {
+        set((state) => ({ course: [course, ...state.courses] }))
+    },
+
+    removeCourse: (courseId) => {
+        set((state) => ({ course: state.courses.filter((course) => course.id !== courseId) }))
+    },
+
+    toggleStatus: (courseId) => {
+        set((state) => ({
+            courses: state.courses.map((course) => course.id === courseId ? { ...course, completed: !course.completed } : course)
+        }))
+    }
+})
+
+const useCourseStore = create(devtools
+    (persist
+        (courseStore, { name: "courses" })
+    )
+)
+
+export default useCourseStore;
